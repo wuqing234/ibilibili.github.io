f8699c2f62a562ff39dd24c7ff27defb17223755
wuqing234 <33693575+wuqing234@users.noreply.github.com>
2018-08-20 10:03:58 +0800

    0.2

---
+++ zanblog/zanblog/content-download.php
@@ -0,0 +1,24 @@
+<?php
+/*
+Template Name: content-download
+*/
+?>
+
+<?php get_header(); ?>
+<div id="zan-bodyer">
+	<div class="container">
+		<div class="bs-masthead">
+			<h1>Zanblog</h1>
+			<p class="lead">Zanblog是由佚站互联制作的基于Bootstrap3.0的WordPress主题，采用Bootstrap3.0的扁平化设计风格，提供免费下载，希望大家能够喜欢:)
+			</p>
+			<p>
+				<a class="btn btn-large" onclick="_gaq.push(['_trackEvent', 'Jumbotron actions', 'Download', 'Download 3.0.0']);" href="http://pan.baidu.com/share/link?shareid=3082720944&uk=3291201722">下载Zanblog(提取密码tura)</a>
+			
+				<a class="btn btn-large" onclick="_gaq.push(['_trackEvent', 'Jumbotron actions', 'Download', 'Download 3.0.0']);" href="http://www.yeahzan.com/zanblog/archives/131.html">使用说明</a>
+			</p>
+		</div>
+	</div>
+</div>
+<?php get_footer(); ?>
+</body>
+</html>
